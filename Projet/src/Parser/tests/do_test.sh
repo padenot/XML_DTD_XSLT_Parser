05f001c1d0e937e77bc60d2b210419b096bacc86
fenrhil <fenrhil@gmail.com>
2011-04-20 17:37:26 +0200

    Test modifications.

--- Projet/src/Parser/tests/do_test.sh
+++ Projet/src/Parser/tests/do_test.sh
@@ -17,8 +17,8 @@ function find_xml_files ()
 function output_redirection ()
 {
 # $1 : file beeing tested
-	echo '1>/dev/null' '2>&1'
-	echo
+#	echo '1>/dev/null' '2>&1'
+echo
 }
 
 for bundle in $(find . -maxdepth 1 -name 'bundle*' -type d | sort)
@@ -28,32 +28,40 @@ do
 	cd $bundle
 
 	minor_sep
-	cd valid
-	for valid in $(find_xml_files)
-	do
-		eval ../../$PARSER $valid $(output_redirection $PWD/$valid)
-		if (( $? == 0 ))
-		then
-			echo "File $bundle/valid/$valid: ok"
-		else
-			echo "File $bundle/valid/$valid: FAILURE"
-		fi
-	done
-	cd ..
+	if cd valid
+	then
+		for valid in $(find_xml_files)
+		do
+			eval ../../$PARSER $valid $(output_redirection $PWD/$valid)
+			if (( $? == 0 ))
+			then
+				echo "File $bundle/valid/$valid: ok"
+			else
+				echo "File $bundle/valid/$valid: FAILURE"
+			fi
+		done
+		cd ..
+	else
+		echo "No 'valid' directory."
+	fi
 
 	minor_sep
-	cd invalid
-	for invalid in $(find_xml_files)
-	do
-		eval ../../$PARSER $invalid $(output_redirection $PWD/$invalid)
-		if (( $? == 0 ))
-		then
-			echo "File $bundle/invalid/$invalid: FAILURE"
-		else
-			echo "File $bundle/invalid/$invalid: ok"
-		fi
-	done
-	cd ..
+	if cd invalid
+	then
+		for invalid in $(find_xml_files)
+		do
+			eval ../../$PARSER $invalid $(output_redirection $PWD/$invalid)
+			if (( $? == 0 ))
+			then
+				echo "File $bundle/invalid/$invalid: FAILURE"
+			else
+				echo "File $bundle/invalid/$invalid: ok"
+			fi
+		done
+		cd ..
+	else
+		echo "No 'invalid' directory."
+	fi
 
 	cd ..
 done
